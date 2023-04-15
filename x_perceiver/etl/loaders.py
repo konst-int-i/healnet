744779f3d6f70ac57085699c765870872a2a1b2d
konst-int-i <konstantin.hemker@googlemail.com>
2023-04-15 15:13:57 +0100

    ETL: update dataloader to take the data sources as list argument

--- x_perceiver/etl/loaders.py
+++ x_perceiver/etl/loaders.py
@@ -17,7 +17,7 @@ from box import Box
 
 class TCGADataset(Dataset):
 
-    def __init__(self, dataset: str, config: Box, preload_tensors: bool = True):
+    def __init__(self, dataset: str, config: Box, level: int=3, sources: List = ["molecular", "slides"]):
         """
         Dataset wrapper to load different TCGA data modalities (molecular and WSI data).
         Args:
@@ -35,56 +35,78 @@ class TCGADataset(Dataset):
             >>> slide, tensor = dataset.load_wsi(blca.sample_slide_id, resolution="lowest")
         """
         self.dataset = dataset
+        self.sources = sources
+        valid_sources = ["molecular", "slides"]
+        assert all([source in valid_sources for source in sources]), f"Invalid source specified. Valid sources are {valid_sources}"
         self.config = config
         self.data_conf = config.data
         self.molecular_df = self.load_molecular()
-        # filter for image samples available
+        self.level = level
+        self.slide_idx: dict = self._get_slide_idx() # {idx (molecular_df): slide_id}
         self.wsi_paths: dict = self._get_slide_dict() # {slide_id: path}
+        self.wsi_width, self.wsi_height = self.get_resize_dims(level=self.level)
         self.target = self.molecular_df["high_risk"]
         self.features = self.molecular_df.drop(["site", "oncotree_code", "case_id", "slide_id"], axis=1)
         # self.molecular_filtered_df = self.molecular_df.loc[self.molecular_df["slide_id"].isin(self.svs_dict.keys())]
-        self.slide_idx: dict = self._get_slide_idx() # {idx (molecular_df): slide_id}
         self.get_info(full_detail=False)
-        if preload_tensors:
-            self.wsi_tensors = self.load_transform_wsi(level=3) # {slide_id: tensor}
+        # if preload_tensors:
+        #     self.wsi_tensors = self.load_transform_wsi(level=3) # {slide_id: tensor}
 
     def __getitem__(self, index):
+        # TODO - implement cache
         slide_id = self.molecular_df.iloc[index]["slide_id"]
         # # check that slide is available
-        if slide_id in self.wsi_paths.keys():
-            slide_tensor = self.wsi_tensors[slide_id]
-        else:
-            slide, slide_tensor = None, torch.tensor([])
-        # filter out features not relevant for training
-        # df = self.molecular_df.drop(["site", "oncotree_code", "case_id", "slide_id"], axis=1)
-        mol_tensor = torch.from_numpy(self.features.iloc[index].values)
         label = self.target.iloc[index]
-        return mol_tensor, slide_tensor, label
+        if len(self.sources) == 1 and self.sources[0] == "molecular":
+            mol_tensor = torch.from_numpy(self.features.iloc[index].values)
+            return mol_tensor, label
+        elif len(self.sources) == 1 and self.sources[0] == "slides":
+            slide, slide_tensor = self.load_wsi(slide_id, level=self.level)
+            return slide_tensor, label
+        else: # both
+            slide, slide_tensor = self.load_wsi(slide_id, level=self.level)
+            mol_tensor = torch.from_numpy(self.features.iloc[index].values)
+            return (mol_tensor, slide_tensor), label
 
-    def load_transform_wsi(self, level: int):
-        widths = []
-        heights = []
-        tensor_dict = {}
-        print("Loading WSIs...")
-        # for key, value in list(self.wsi_paths.items())[0:2]:
-        for key, value in self.wsi_paths.items():
-            print(f"Reading WSI: {key}")
-            slide, array, read_level = self.load_wsi(key, level=level)
-            tensor_dict[key] = array
-            widths.append(slide.level_dimensions[read_level][0])
-            heights.append(slide.level_dimensions[read_level][1])
-        largest_index = np.argmax(widths)
+
+    def get_resize_dims(self, level: int):
+        # TODO - validate which dimensions to pick for each level
+        slide_sample = self.slide_idx[0]
+        slide_path = self.wsi_paths[slide_sample]
+        slide = OpenSlide(slide_path)
+        print(slide.level_dimensions[level])
+        width = slide.level_dimensions[level][0]
+        height = slide.level_dimensions[level][1]
         # take nearest multiple of 128 of height and width (for patches)
-        width = round(widths[largest_index]/128)*128
-        height = round(heights[largest_index]/128)*128
-        # get highest resolution given the level (not all slides have the same level dimensions)
-        print(f"Transforming WSIs to (w:{width}, h:{height}) tensors...")
-        transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize((height, width)),
-        ])
-        transformed_dict = {key: transform(value) for key, value in tensor_dict.items()}
-        return transformed_dict
+        width = round(width/128)*128
+        height = round(height/128)*128
+        return width, height
+
+
+    # def load_transform_wsi(self, level: int):
+    #     widths = []
+    #     heights = []
+    #     tensor_dict = {}
+    #     print("Loading WSIs...")
+    #     # for key, value in list(self.wsi_paths.items())[0:2]:
+    #     for key, value in self.wsi_paths.items():
+    #         print(f"Reading WSI: {key}")
+    #         slide, array, read_level = self.load_wsi(key, level=level)
+    #         tensor_dict[key] = array
+    #         widths.append(slide.level_dimensions[read_level][0])
+    #         heights.append(slide.level_dimensions[read_level][1])
+    #     largest_index = np.argmax(widths)
+    #     # take nearest multiple of 128 of height and width (for patches)
+    #     width = round(widths[largest_index]/128)*128
+    #     height = round(heights[largest_index]/128)*128
+    #     # get highest resolution given the level (not all slides have the same level dimensions)
+    #     print(f"Transforming WSIs to (w:{width}, h:{height}) tensors...")
+    #     transform = transforms.Compose([
+    #         transforms.ToTensor(),
+    #         transforms.Resize((height, width)),
+    #     ])
+    #     transformed_dict = {key: transform(value) for key, value in tensor_dict.items()}
+    #     return transformed_dict
 
     def _get_slide_idx(self):
         return dict(zip(self.molecular_df.index, self.molecular_df["slide_id"]))
@@ -110,7 +132,7 @@ class TCGADataset(Dataset):
         return next(iter(self.wsi_paths.keys()))
 
     def get_info(self, full_detail: bool = False):
-        slide, tensor, _ = self.load_wsi(self.sample_slide_id, resolution="lowest")
+        slide, tensor = self.load_wsi(self.sample_slide_id, level=self.level)
         slide_path = Path(self.config.tcga_path).joinpath(f"wsi/{self.dataset}/")
         print(f"Dataset: {self.dataset.upper()}")
         print(f"Molecular data shape: {self.molecular_df.shape}")
@@ -121,6 +143,8 @@ class TCGADataset(Dataset):
         # print(f"Slide dimensions: {slide.dimensions}")
         print(f"Slide level count: {slide.level_count}")
         print(f"Slide level dimensions: {slide.level_dimensions}")
+        print(f"Slide resize dimensions: w: {self.wsi_width}, h: {self.wsi_height}")
+        print(f"Sources selected: {self.sources}")
 
         if full_detail:
             pprint(dict(slide.properties))
@@ -166,11 +190,17 @@ class TCGADataset(Dataset):
                 level = int(slide.level_count / 2)
         if level > slide.level_count - 1:
             level = slide.level_count - 1
-        print(f"Loading slide at level {level} ")
+        # print(f"Loading slide at level {level} ")
         # load in region
         size = slide.level_dimensions[level]
         region = slide.read_region((0,0), level, size)
-        return slide, region, level
+
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize((self.wsi_height, self.wsi_width)),
+        ])
+
+        return slide, transform(region)
 
 
 
