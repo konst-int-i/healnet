78ded47a1ea72463ccf0fc3311e9a7a075cfdef5
konst-int-i <konstantin.hemker@googlemail.com>
2023-05-09 12:54:23 +0100

    INIT: add losses

--- x_perceiver/models/__init__.py
+++ x_perceiver/models/__init__.py
@@ -1,4 +1,4 @@
 from x_perceiver.models.perceiver import Perceiver
-from x_perceiver.models.survival_loss import NLLSurvLoss
+from x_perceiver.models.survival_loss import NLLSurvLoss, CrossEntropySurvLoss, CoxPHSurvLoss
 
-__all__ = ["Perceiver", "NLLSurvLoss"]
+__all__ = ["Perceiver", "NLLSurvLoss", "CrossEntropySurvLoss", "CoxPHSurvLoss"]
