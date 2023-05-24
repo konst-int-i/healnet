4775cdcc9ea5ec22fdc5a9ee904e6d708d13c8ec
konst-int-i <konstantin.hemker@googlemail.com>
2023-05-24 14:54:52 +0100

    CONFIG: add patch size for preprocessing script

--- tasks.py
+++ tasks.py
@@ -23,14 +23,14 @@ def install(c, system: str):
     os.remove("gdc-client.zip")
 
 @task
-def download(c, dataset:str, config_path:str, samples: int = None):
+def download(c, dataset:str, config:str, samples: int = None):
     valid_datasets = ["brca", "blca"]
-    config = Config(config_path).read()
-    download_dir = Path(config.tcga_path).joinpath(f"wsi/{dataset}")
+    conf = Config(config).read()
+    download_dir = Path(conf.tcga_path).joinpath(f"wsi/{dataset}")
 
     assert dataset in valid_datasets, f"Invalid dataset arg, must be one of {valid_datasets}"
 
-    manifest_path = Path(config.tcga_path).joinpath(f"gdc_manifests/filtered/{dataset}_wsi_manifest_filtered.txt")
+    manifest_path = Path(conf.tcga_path).joinpath(f"gdc_manifests/filtered/{dataset}_wsi_manifest_filtered.txt")
     # Download entire manifest unless specified otherwise
     if samples is not None:
         manifest = pd.read_csv(manifest_path, sep="\t")
@@ -38,33 +38,55 @@ def download(c, dataset:str, config_path:str, samples: int = None):
         tmp_path = manifest_path.parent.joinpath(f"{dataset}_tmp.txt")
         manifest.to_csv(tmp_path, sep="\t", index=False)
         print(f"Downloading {manifest.shape[0]} files from {dataset} dataset...")
-        c.run(f"{config.gdc_client} download -m {tmp_path} -d {download_dir}")
+        c.run(f"{conf.gdc_client} download -m {tmp_path} -d {download_dir}")
         # cleanupJ
         os.remove(tmp_path)
 
     else:
-        c.run(f"{config.gdc_client} download -m {manifest_path} -d {download_dir}")
+        c.run(f"{conf.gdc_client} download -m {manifest_path} -d {download_dir}")
 
     # flatten directory structure (required to easily run CLAM preprocessing)
-    flatten(c, dataset, config_path)
+    flatten(c, dataset, config)
 
 @task
-def flatten(c, dataset, config_path):
+def flatten(c, dataset: str, config: str):
     """
     Flattens directory structure for WSI images after download using the GDC client from
      `data_dir/*.svs` instead of `data_dir/hash_subdir/*.svs`.
     Args:
         c:
         dataset:
-        config_path:
+        config:
 
     Returns:
     """
-    config = Config(config_path).read()
-    download_dir = Path(config.tcga_path).joinpath(f"wsi/{dataset}")
+    conf = Config(config).read()
+    download_dir = Path(conf.tcga_path).joinpath(f"wsi/{dataset}")
     # flatten directory structure
     c.run(f"find {download_dir} -type f -name '*.svs' -exec mv {{}} {download_dir} \;")
     # remove everything that's not a .svs file
     c.run(f"find {download_dir} ! -name '*.svs' -delete")
 
+@task
+def preprocess(c, dataset: str, config: str, level: int):
+    """
+    Preprocesses WSI images for downstream tasks.
+    Args:
+        c:
+        dataset:
+        config:
+
+    Returns:
+
+    """
+    conf = Config(config).read()
+    raw_dir = Path(conf.tcga_path).joinpath(f"wsi/{dataset}")
+    preprocessed_dir = Path(conf.tcga_path).joinpath(f"wsi/{dataset}_preprocessed_level{level}")
+
+    # clone CLAM repo if doesn't exist
+    if not os.path.exists("CLAM/"):
+        c.run("git clone git@github.com:mahmoodlab/CLAM.git")
+
+    c.run(f"python CLAM/create_patches_fp.py --source {raw_dir} --save_dir {preprocessed_dir} "
+          f"--patch_size {config['data.patch_size']} --patch_level {level} --seg --patch --stitch")
 
