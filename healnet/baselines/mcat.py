d5a1f697319b9c5f28d37a545a00a8c88f192385
konst-int-i <konstantin.hemker@googlemail.com>
2023-08-08 14:07:26 +0100

    BASELINES: multi-modal MCAT model running

--- healnet/baselines/mcat.py
+++ healnet/baselines/mcat.py
@@ -4,6 +4,7 @@ Multimodal Co-Attention Transformer for Survival Prediction in Gigapixel Whole S
 Mostly based on their GitHub repository and adjusted for our pipeline.
 https://github.com/mahmoodlab/MCAT/tree/master
 """
+import einops
 import numpy as np
 
 import torch
@@ -29,26 +30,40 @@ import warnings
 # from models.model_utils import *
 
 
-class MCAT_Surv(nn.Module):
-    def __init__(self, fusion='concat', omic_sizes=[100, 200, 300, 400, 500, 600], n_classes=4,
+class MCAT(nn.Module):
+    def __init__(self, omic_shape: Tuple, wsi_shape: Tuple, fusion='concat', n_classes=4,
                  model_size_wsi: str='small', model_size_omic: str='small', dropout=0.25):
-        super(MCAT_Surv, self).__init__()
+        """
+        Multimodal Co-Attention Transformer
+        Args:
+            fusion:
+            omic_shape:
+            n_classes:
+            model_size_wsi:
+            model_size_omic:
+            dropout:
+        """
+        super(MCAT, self).__init__()
         self.fusion = fusion
-        self.omic_sizes = omic_sizes
+        self.omic_shape = omic_shape
+        self.wsi_shape = wsi_shape
         self.n_classes = n_classes
         self.size_dict_WSI = {"small": [1024, 256, 256], "big": [1024, 512, 384]}
         self.size_dict_omic = {'small': [256, 256], 'big': [1024, 1024, 1024, 256]}
 
         ### FC Layer over WSI bag
         size = self.size_dict_WSI[model_size_wsi]
+        size[0] = wsi_shape[0] # other embedding dims
+        # fc = [nn.Linear(wsi_shape[0], 256), nn.ReLU()]
         fc = [nn.Linear(size[0], size[1]), nn.ReLU()]
+        # wsi_size
         fc.append(nn.Dropout(0.25))
         self.wsi_net = nn.Sequential(*fc)
 
         ### Constructing Genomic SNN
         hidden = self.size_dict_omic[model_size_omic]
         sig_networks = []
-        for input_dim in omic_sizes:
+        for input_dim in self.omic_shape:
             fc_omic = [SNN_Block(dim1=input_dim, dim2=hidden[0])]
             for i, _ in enumerate(hidden[1:]):
                 fc_omic.append(SNN_Block(dim1=hidden[i], dim2=hidden[i+1], dropout=0.25))
@@ -62,6 +77,7 @@ class MCAT_Surv(nn.Module):
         path_encoder_layer = nn.TransformerEncoderLayer(d_model=256, nhead=8, dim_feedforward=512, dropout=dropout, activation='relu')
         self.path_transformer = nn.TransformerEncoder(path_encoder_layer, num_layers=2)
         self.path_attention_head = Attn_Net_Gated(L=size[2], D=size[2], dropout=dropout, n_classes=1)
+
         self.path_rho = nn.Sequential(*[nn.Linear(size[2], size[2]), nn.ReLU(), nn.Dropout(dropout)])
 
         ### Omic Transformer + Attention Head
@@ -81,13 +97,18 @@ class MCAT_Surv(nn.Module):
         ### Classifier
         self.classifier = nn.Linear(size[2], n_classes)
 
-    def forward(self, **kwargs):
-        x_path = kwargs['x_path']
-        x_omic = [kwargs['x_omic%d' % i] for i in range(1,7)]
-
-        h_path_bag = self.wsi_net(x_path).unsqueeze(1) ### path embeddings are fed through a FC layer
+    def forward(self, data, **kwargs):
+        # x_path = kwargs['x_path']
+        # x_omic = [kwargs['x_omic%d' % i] for i in range(1,7)]
+        x_omic = [data[0]] # needs to be wrapped in list (used to expect multiple omic sources)
         h_omic = [self.sig_networks[idx].forward(sig_feat) for idx, sig_feat in enumerate(x_omic)] ### each omic signature goes through it's own FC layer
-        h_omic_bag = torch.stack(h_omic).unsqueeze(1) ### omic embeddings are stacked (to be used in co-attention)
+
+        # h_omic_bag = torch.stack(h_omic).unsqueeze(1)
+        h_omic_bag = torch.stack(h_omic)  ### omic embeddings are stacked (to be used in co-attention)
+
+        x_path = data[1]
+        x_path = einops.rearrange(x_path, "b dim patches -> b patches dim")
+        h_path_bag = self.wsi_net(x_path).unsqueeze(1) ### path embeddings are fed through a FC layer
 
         # Coattn
         h_path_coattn, A_coattn = self.coattn(h_omic_bag, h_path_bag, h_path_bag)
@@ -96,14 +117,18 @@ class MCAT_Surv(nn.Module):
         h_path_trans = self.path_transformer(h_path_coattn)
         A_path, h_path = self.path_attention_head(h_path_trans.squeeze(1))
         A_path = torch.transpose(A_path, 1, 0)
-        h_path = torch.mm(F.softmax(A_path, dim=1) , h_path)
+        A_path = A_path.squeeze(-1)
+        h_path = h_path.squeeze(0)
+        h_path = torch.mm(F.softmax(A_path.t(), dim=1) , h_path)
         h_path = self.path_rho(h_path).squeeze()
 
         ### Omic
         h_omic_trans = self.omic_transformer(h_omic_bag)
         A_omic, h_omic = self.omic_attention_head(h_omic_trans.squeeze(1))
         A_omic = torch.transpose(A_omic, 1, 0)
-        h_omic = torch.mm(F.softmax(A_omic, dim=1) , h_omic)
+        A_omic = A_omic.squeeze(-1)
+        h_omic = h_omic.squeeze(0)
+        h_omic = torch.mm(F.softmax(A_omic.t(), dim=1), h_omic)
         h_omic = self.omic_rho(h_omic).squeeze()
 
         if self.fusion == 'bilinear':
@@ -112,14 +137,16 @@ class MCAT_Surv(nn.Module):
             h = self.mm(torch.cat([h_path, h_omic], axis=0))
 
         ### Survival Layer
-        logits = self.classifier(h).unsqueeze(0)
-        Y_hat = torch.topk(logits, 1, dim = 1)[1]
-        hazards = torch.sigmoid(logits)
-        S = torch.cumprod(1 - hazards, dim=1)
+        logits = self.classifier(h)
 
-        attention_scores = {'coattn': A_coattn, 'path': A_path, 'omic': A_omic}
+        # happens in train loop
+        # Y_hat = torch.topk(logits, 1, dim = 1)[1]
+        # hazards = torch.sigmoid(logits)
+        # S = torch.cumprod(1 - hazards, dim=1)
+        #
+        # attention_scores = {'coattn': A_coattn, 'path': A_path, 'omic': A_omic}
 
-        return hazards, S, Y_hat, attention_scores
+        return logits
 
 
     def captum(self, x_path, x_omic1, x_omic2, x_omic3, x_omic4, x_omic5, x_omic6):
