fe2f116ab83d6715c7df90d14b77f4fceb8b61f6
konst-int-i <konstantin.hemker@googlemail.com>
2023-10-31 16:56:54 +0000

    Big commit: removes old files

--- healnet/etl/__init__.py
+++
@@ -1,3 +0,0 @@
-from healnet.etl.loaders import TCGADataset
-
-__all__ = ["TCGADataset"]
--- healnet/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from healnet.models.healnet import HealNet
-from healnet.baselines import FCNN
-from healnet.models.survival_loss import NLLSurvLoss, CrossEntropySurvLoss, CoxPHSurvLoss
-
-__all__ = ["NLLSurvLoss", "CrossEntropySurvLoss", "CoxPHSurvLoss", "FCNN", "HealNet"]
