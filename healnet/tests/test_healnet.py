f0f9f37a06d7418f81310c7aef3fb9327ac7f498
konst-int-i <konstantin.hemker@googlemail.com>
2023-11-01 12:18:15 +0000

    TESTS: add regular attention test

--- healnet/tests/test_healnet.py
+++ healnet/tests/test_healnet.py
@@ -16,16 +16,15 @@ def shape_test_vars():
     # latent_dim
     query = torch.randn(b, c_n, c_d)
     latent = torch.randn(b, l_n, l_d)
-    print("\n")
-    print("query", query.shape)
-    print("context", latent.shape)
-
     return b, c_n, c_d, l_n, l_d, query, latent
 
 
 def test_latent_cross_attention(shape_test_vars):
 
     b, c_n, c_d, l_n, l_d, query, latent = shape_test_vars
+    print("\nquery", query.shape)
+    print("context", latent.shape)
+
 
     # latent cross attention
     lc_attention = LatentCrossAttention(query_dim=c_n, latent_dim=l_d)
@@ -42,3 +41,15 @@ def test_attention_update(shape_test_vars):
     attention_update = AttentionUpdate(c_n=c_d, l_d=l_d)
     updated_context = attention_update(x=query, context=latent)
     assert updated_context.shape == (b, l_n, l_d)
+
+def test_attention(shape_test_vars):
+    b, c_n, c_d, l_n, l_d, query, latent = shape_test_vars
+    # attention
+    attention = Attention(query_dim=l_d, context_dim=c_d)
+    # NOTE - traditional attention expectes the latent as the query and returns the latent
+    # Problem is that this also means that the attention-matrix is at the latent level
+    updated_latent = attention(x=latent, context=query)
+
+    assert updated_latent.shape == (b, l_n, l_d)
+
+
