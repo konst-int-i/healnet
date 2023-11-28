0cb48a64edbb89bee99a2910e138a0a16de8d6de
konst-int-i <konstantin.hemker@googlemail.com>
2023-11-28 10:52:02 +0000

    PIP: add package_dir

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='healnet',
     version='0.0.1',
+    package_dir={'': 'healnet'},
     packages=find_packages(),
     install_requires=[
         'einops',
