4c0a425eabb539fea1bc8e1af03afc33590ebc3b
konst-int-i <konstantin.hemker@googlemail.com>
2023-11-28 10:37:39 +0000

    PIP: add minimal requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ setup(
     name='healnet',
     version='0.0.1',
     packages=find_packages(),
-    install_requires=[]
+    install_requires=[
+        'einops',
+        'torch',
+        'torchvision',
+        'openslide-python',
+        'seaborn',
+        'pandas'
+    ]
 )
