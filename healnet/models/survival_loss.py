15e5e3d26344cef8806aa39fb04790047dd2be49
konst-int-i <konstantin.hemker@googlemail.com>
2023-12-13 17:14:35 -0600

    LOSS: remove wrapper and add some docs

--- healnet/models/survival_loss.py
+++ healnet/models/survival_loss.py
@@ -7,12 +7,24 @@ import torch.nn.functional as F
 
 
 def nll_loss(hazards, S, Y, c, weights=None, alpha=0.4, eps=1e-7):
+    """
+    hazards: (n_batches, n_classes)
+        The neural network output discrete survival predictions such that hazards = sigmoid(h).
+    Y: (n_batches, 1)
+        The true time bin index label.
+    c: (n_batches, 1)
+        The censoring status indicator.
+    alpha: float
+    eps: float
+        Numerical constant; lower bound to avoid taking logs of tiny numbers.
+    reduction: str
+        Do we sum or average the loss function over the batches. Must be one of ['mean', 'sum']
+    """
     batch_size = len(Y)
     Y = Y.view(batch_size, 1) # ground truth bin, 1,2,...,k
     c = c.view(batch_size, 1).float() #censorship status, 0 or 1
     if S is None:
         S = torch.cumprod(1 - hazards, dim=1) # surival is cumulative product of 1 - hazards
-    # without padding, S(0) = S[0], h(0) = h[0]
     S_padded = torch.cat([torch.ones_like(c), S], 1) #S(-1) = 0, all patients are alive from (-inf, 0) by definition
     # after padding, S(0) = S[1], S(1) = S[2], etc, h(0) = h[0]
     uncensored_loss = -(1 - c) * (torch.log(torch.gather(S_padded, 1, Y).clamp(min=eps)) + torch.log(torch.gather(hazards, 1, Y).clamp(min=eps)))
@@ -26,55 +38,13 @@ def nll_loss(hazards, S, Y, c, weights=None, alpha=0.4, eps=1e-7):
         gathered_weights = torch.gather(weights, 1, Y)
         neg_l *= gathered_weights
 
-
     loss = (1-alpha) * neg_l + alpha * uncensored_loss
     loss = loss.mean()
     return loss
 
-
-class NLLSurvLoss(nn.Module):
-    """
-    The negative log-likelihood loss function for the discrete time to event model (Zadeh and Schmid, 2020).
-    Code borrowed from https://github.com/mahmoodlab/Patch-GCN/blob/master/utils/utils.py
-    Parameters
-    ----------
-    alpha: float
-    eps: float
-        Numerical constant; lower bound to avoid taking logs of tiny numbers.
-    reduction: str
-        Do we sum or average the loss function over the batches. Must be one of ['mean', 'sum']
-    """
-    def __init__(self, alpha=0.0, eps=1e-7, reduction='mean'):
-        super().__init__()
-        self.alpha = alpha
-        self.eps = eps
-        self.reduction = reduction
-
-    def __call__(self, h, y, c):
-        """
-        Parameters
-        ----------
-        h: (n_batches, n_classes)
-            The neural network output discrete survival predictions such that hazards = sigmoid(h)
-        y: (n_batches, 1): True time bin index label (q_bin of survival_months)
-        c: (n_batches, 1): censoring status indicator
-        """
-        # return None
-        return nll_loss_alternative(h=h,
-                        y=y.unsqueeze(dim=1),
-                        c=c.unsqueeze(dim=1),
-                        alpha=self.alpha, eps=self.eps,
-                        reduction=self.reduction)
-        # return nll_loss_porpoise(hazards=h,
-        #                         Y=y.unsqueeze(dim=1),
-        #                         c=c.unsqueeze(dim=1),
-        #                         alpha=self.alpha, eps=self.eps,
-        #                         S=None)
-
 def nll_loss_alternative(h, y, c, alpha=0.0, eps=1e-7, reduction='mean'):
     """
     The negative log-likelihood loss function for the discrete time to event model (Zadeh and Schmid, 2020).
-    Code borrowed from https://github.com/mahmoodlab/Patch-GCN/blob/master/utils/utils.py
     Parameters
     ----------
     h: (n_batches, n_classes)
