6f8b5ff570982e3b219a3144a8720e64baec242b
konst-int-i <konstantin.hemker@googlemail.com>
2024-01-22 10:21:51 +0000

    TESTS: fix healnet smoke tests

--- healnet/models/__init__.py
+++ healnet/models/__init__.py
@@ -1,5 +1,5 @@
-from healnet.models.healnet import HealNet
+from healnet.models.healnet import HealNet, Attention
 from healnet.baselines import FCNN
 from healnet.models.survival_loss import CrossEntropySurvLoss, CoxPHSurvLoss
 
-__all__ = ["CrossEntropySurvLoss", "CoxPHSurvLoss", "FCNN", "HealNet"]
+__all__ = ["CrossEntropySurvLoss", "CoxPHSurvLoss", "FCNN", "HealNet", "Attention"]
